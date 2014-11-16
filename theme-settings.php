27b148e9335465f61e614b1b13962a609775c6b9
Sergey Pavlenko <sergeypavlenko@1343804.no-reply.drupal.org>
2014-11-16 08:38:25 +0200

    Edit comment to function.

--- theme-settings.php
+++ theme-settings.php
@@ -5,7 +5,7 @@
  */
 
 /**
- * Prameter definitions for Bootstrap Instant.
+ * Implements hook_form_system_theme_settings_alter().
  */
 function bootstrap_instant_form_system_theme_settings_alter(&$form, $form_state, $form_id = NULL) {
   // Work-around for a core bug affecting admin themes. See issue #943212.
